e04bae2b3c058f012eb4a3e697c2294e13332ecb
Damiruly Gafur <sholpandaniel@yandex.ru>
2024-07-06 20:36:24 +0500

    Added .env token

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-  };
-  
-  // Если вы планируете использовать Sentry в будущем, вы можете раскомментировать эти строки
-  // import { withSentryConfig } from '@sentry/nextjs';
-  // const sentryWebpackPluginOptions = {
-  //   silent: true,
-  //   org: "leadai-e0",
-  //   project: "leadai",
-  //   widenClientFileUpload: true,
-  //   transpileClientSDK: true,
-  //   hideSourceMaps: true,
-  //   disableLogger: true,
-  //   automaticVercelMonitors: true,
-  // };
-  // export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
+
+export default nextConfig;
