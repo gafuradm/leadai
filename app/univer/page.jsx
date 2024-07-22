96e44b9aaeaeb1c9012596e36871121a155ed769
Damiruly Gafur <sholpandaniel@yandex.ru>
2024-07-22 14:21:06 +0500

    Changed func

--- app/univer/page.jsx
+++ app/univer/page.jsx
@@ -34,7 +34,7 @@ export default function Home() {
     setRecommendations("Loading recommendations...");
     setLoading(true);
     try {
-      const response = await fetch('/api/getRecommendations', {
+      const response = await fetch('../api/getRecommendations', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData)
